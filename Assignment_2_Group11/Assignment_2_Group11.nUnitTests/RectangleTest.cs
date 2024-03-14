9cc909b3d3d336dacd7f6f710c31d3b1dadfb044
Sukhwinder Singh <sukhwindernoorpur@gmail.com>
2024-03-14 02:50:54 -0400

    Test cases for parametrizedConstructor,Set length,Set width,GetPerimeter,Get Area

--- Assignment_2_Group11/Assignment_2_Group11.nUnitTests/RectangleTest.cs
+++ Assignment_2_Group11/Assignment_2_Group11.nUnitTests/RectangleTest.cs
@@ -32,5 +32,85 @@ namespace Assignment_2_Group11.Tests
             Assert.AreEqual(width, actualWidth);
         }
 
+        [Test]
+        public void ParameterizedConstructor_LengthAndWidthSetCorrectly()
+        {
+
+            // Arrange
+            int length = 7;
+            int width = 10;
+            var rectangle = new Rectangle(length, width);
+
+            //Act
+            int actualLength = rectangle.GetLength();
+            int actualWidth = rectangle.GetWidth();
+
+            // Assert
+            Assert.AreEqual(length, actualLength);
+            Assert.AreEqual(width, actualWidth);
+        }
+
+        [Test]
+        public void SetLength_ValidValue_LengthUpdated()
+        {
+
+            //Arrange
+            int newLength = 12;
+            _rectangle.SetLength(newLength);
+
+            //Act
+            int actualLength = _rectangle.GetLength();
+
+            //Assert
+            Assert.AreEqual(newLength, actualLength);
+        }
+
+        [Test]
+        public void SetWidth_ValidValue_WidthUpdated()
+        {
+            // Arrange
+            int newWidth = 6;
+            _rectangle.SetWidth(newWidth);
+
+            //Act
+            int actualLength = _rectangle.GetWidth();
+
+            //Assert
+            Assert.AreEqual(newWidth, actualLength);
+        }
+
+        [Test]
+        public void GetPerimeter_ValidValues_CorrectPerimeterCalculated()
+        {
+            //Arrange
+            int length = 5;
+            int width = 8;
+            _rectangle.SetLength(length);
+            _rectangle.SetWidth(width);
+
+            //Act
+            int actualPerimeter = _rectangle.GetPerimeter();
+
+            //Assert
+            Assert.AreEqual(2 * (length + width), actualPerimeter);
+        }
+
+        [Test]
+        public void GetArea_ValidValues_CorrectAreaCalculated()
+        {
+            // Arrange
+            int length = 4;
+            int width = 9;
+            _rectangle.SetLength(length);
+            _rectangle.SetWidth(width);
+
+            // Act
+            int actualArea = _rectangle.GetArea();
+
+            // Assert
+            Assert.AreEqual(length * width, actualArea);
+        }
+
+
     }
 }
