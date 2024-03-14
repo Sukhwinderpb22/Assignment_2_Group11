e4ddeb9723b223141c886fc15ce19319c52ca7e5
Sukhwinder Singh <sukhwindernoorpur@gmail.com>
2024-03-14 02:45:20 -0400

    Add project files.

---
+++ Assignment_2_Group11/Assignment_2_Group11.nUnitTests/RectangleTest.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Assignment_2_Group11.Tests
+{
+
+    public class RectangleTest
+    {
+        private Rectangle _rectangle = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            _rectangle = new Rectangle();
+        }
+
+        [Test]
+        public void DefaultConstructor_LengthAndWidthSetToOne()
+        {
+            // Arrange
+            int length = 1;
+            int width = 1;
+            var rectangle = new Rectangle();
+
+            //Act
+            int actualLength = rectangle.GetLength();
+            int actualWidth = rectangle.GetWidth();
+
+            // Assert
+            Assert.AreEqual(length, actualLength);
+            Assert.AreEqual(width, actualWidth);
+        }
+
+    }
+}
---
+++ Assignment_2_Group11/Assignment_2_Group11/Rectangle.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment_2_Group11;
+
+namespace Assignment_2_Group11
+{
+
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+         
+        public void SetLength(int length)
+        {
+            this.length = length;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public void SetWidth(int width)
+        {
+            this.width = width;
+        }
+
+        public int GetPerimeter()
+        {
+            return 2 * (length + width);
+        }
+
+        public int GetArea()
+        {
+            return length * width;
+        }
+    }
+}
